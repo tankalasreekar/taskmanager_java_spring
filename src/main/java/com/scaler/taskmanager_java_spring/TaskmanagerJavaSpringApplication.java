27d7f8d5218964d5852fbe52285aa3f82bfd22c4
tankalasreekar <tankalasrkr@gmail.com>
2024-01-18 00:19:36 +0530

    initial project setup for task manager application

---
+++ src/main/java/com/scaler/taskmanager_java_spring/TaskmanagerJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanager_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/taskmanager_java_spring/TaskmanagerJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanager_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
